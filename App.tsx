aa8739e8b27cedd1667df8dc570e5a3728654265
Rahul Shah <rshah98626@gmail.com>
2022-12-16 18:49:32 -0600

    feat: navigation to modal completed (#1)
    * feat: navigation to modal completed
    
    * PR template

--- App.tsx
+++ App.tsx
@@ -1,13 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+type RootStackParamList = {
+  Home: undefined,
+  LessonsModal: undefined
+}
+const RootStack = createNativeStackNavigator<RootStackParamList>()
 
 export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootStack.Navigator screenOptions={{ headerShown: false }}>
+          <RootStack.Group>
+            <RootStack.Screen name="Home" component={HomeScreen} />
+            <RootStack.Screen name="LessonsModal" component={LessonsModal} options={{ presentation: 'modal' }} />
+          </RootStack.Group>
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+const HomeScreen = ({ navigation }: HomeProps) => {
   return (
     <View style={styles.container}>
       <Text>What's up Dave!</Text>
       <StatusBar style="auto" />
+      <Button title='Go to lesson 1' 
+        onPress={() => navigation.navigate("LessonsModal")}
+      />
     </View>
-  );
+  )
+}
+
+type LessonsModalProps = NativeStackScreenProps<RootStackParamList, 'LessonsModal'>
+const LessonsModal = ({ navigation }: LessonsModalProps) => {
+  return (
+    <View style={styles.container}>
+      <Text>In the modal!</Text>
+      <Button onPress={() => navigation.goBack()} title="Dismiss" />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
